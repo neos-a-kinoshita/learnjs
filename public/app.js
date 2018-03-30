a0f9ac9fd7ba557a334e3cfb4993193f63462852
Akira Kinoshita <a.kinoshita@neoscorp.jp>
2018-03-30 18:54:43 +0900

    create view

--- public/app.js
+++ public/app.js
@@ -2,8 +2,10 @@
 var learnjs = {};
 
 learnjs.problemView = function(problemNumber) {
-  var title = 'Problem #' + problemNumber + ' Coming soon!';
-  return $('<div class="problem-view">').text(title);
+  var title = 'Problem #' + problemNumber;
+  var view = $('.templates .problem-view').clone();
+  view.find('.title').text(title);
+  return view;
 };
 
 learnjs.showView = function(hash) {
