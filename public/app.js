9b98a88fa06cceeff4327004a40c5a7de5d74386
Akira Kinoshita <a.kinoshita@neoscorp.jp>
2018-05-18 14:55:29 +0900

    add nav-bar

--- public/app.js
+++ public/app.js
@@ -56,6 +56,7 @@ learnjs.problemView = function(data) {
 learnjs.showView = function(hash) {
   var routes = {
     '#problem': learnjs.problemView,
+    '#': learnjs.landingView,
     '': learnjs.landingView
   };
   var hashParts = hash.split('-');
