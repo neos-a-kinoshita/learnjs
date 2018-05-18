d370ccdba540f78452eeb815f1ece741c9bb68c2
Akira Kinoshita <a.kinoshita@neoscorp.jp>
2018-05-18 15:50:01 +0900

    add custom event

--- public/app.js
+++ public/app.js
@@ -47,6 +47,15 @@ learnjs.problemView = function(data) {
     return false;
   }
 
+  if (problemNumber < learnjs.problems.length) {
+    var buttonItem = learnjs.template('skip-btn');
+    buttonItem.find('a').attr('href', '#problem-' + (problemNumber + 1));
+    $('.nav-list').append(buttonItem);
+    view.bind('removingView', function() {
+      buttonItem.remove();
+    });
+  }
+
   view.find('.check-btn').click(checkAnswerClick);
   view.find('.title').text(title);
   learnjs.applyObject(problemData, view);
@@ -62,6 +71,7 @@ learnjs.showView = function(hash) {
   var hashParts = hash.split('-');
   var viewFn = routes[hashParts[0]];
   if (viewFn) {
+    learnjs.triggerEvent('removingView', []);
     $('.view-container')
       .empty()
       .append(viewFn(hashParts[1]));
@@ -96,4 +106,8 @@ learnjs.buildCorrectFlash = function(problemNum) {
     link.text("You're Finished!");
   }
   return correctFlash;
+};
+
+learnjs.triggerEvent = function(name, args) {
+  $('.view-container>*').trigger(name, args);
 };
